2135e7fce08baabd36becad5da89cd92843e5468
faeze <faezeebold@gmail.com>
2023-05-09 23:25:36 +0330

    landing-demo

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -3,13 +3,13 @@ import Advertising from './Advertising';
 const Landing = () =>{
     return(
         <>
-        <div className=" w-full inset-y-6   flex flex-row-reverse bg-blue-800 h-38">
+        <div className=" w-full inset-y-6   flex flex-row-reverse bg-blue-800 h-28">
             <div className=" flex flex-rowo m-10">
                 {/* <img src="./p/sib.jpg"></img> */}
                 <p className="text-white text-lg">های دارو</p>
             </div>
 
-            <div className="flex flex-col m-5">
+            <div className="flex flex-col m-2">
                 <p className="text-white">تامین دارو مورد نیاز هر داروخانه</p>
                 <p className=" text-white flex"> <span className="text-yellow-600"> های دارو </span> با سامانه جامع  </p>
                 <button className= "bg-red-500 rounded text-center p-2 mt-2 text-white">ثبت نام/ ورود</button>
@@ -18,11 +18,11 @@ const Landing = () =>{
         </div>
 
 
-    <div  dir="rtl">
+    <div className='flex flex-col'  dir="rtl">
         <div clas className='flex flex-row'>
-           <div>
+           <div className=' w-4/5 '>
             <div className=''>
-                <p className='dir=ltr mt-5 p-3 mr-6  text-teal-600'>داروهای مازاد</p>
+                <p className=' mt-5 p-3 mr-6  text-teal-600'>داروهای مازاد</p>
                 <div className='mr-16'>
                 <Advertising/>
                 </div>
@@ -43,16 +43,34 @@ const Landing = () =>{
            </div>
 
 
-           <div className=' mt-16 border border-gray-500'>
+           <div className=' mt-16 border border-gray-500 w-1/5 m-10'>
             <div>
-                <p className='p-5 w-48 text-center'>دارو های جستجو شده</p>
+                <p className='p-5 text-center '>دارو های جستجو شده</p>
+                <div className='mr-10'>
+                <p>1.استامینوفن</p>
+                <p>2.سفکسیم</p>
+                </div>
             </div>
            </div>
 
         </div>
 
 
-        <div>
+        <div className='bg-slate-500 flex flex-col mt-12 p-3'>
+            <div className='flex flex-row'>
+                <div className='text-white mr-10'>
+                    <p>لینک ها</p>
+                    <p className='mt-2'>درباره ما</p>
+                </div>
+
+                <div className='text-white mr-96 p-3'>
+                    <p>پشتیبانی</p>
+                    <p className='mt-2'>تماش با ما از طریق</p>
+                    <p className='mt-1'> تلفن</p>
+                    <p className='mt-1'>ایمیل</p>
+                </div>
+            </div>
+            <div></div>
         </div>
 
     </div>
