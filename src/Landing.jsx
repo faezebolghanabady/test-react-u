4777758083bd4ae6890d78c85efcc390c9e9c702
faeze <faezeebold@gmail.com>
2023-05-10 22:11:06 +0330

    navbar-update

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -3,13 +3,13 @@ import Advertising from './Advertising';
 const Landing = () =>{
     return(
         <>
-        <div className=" w-full inset-y-6   flex flex-row-reverse bg-blue-800 h-28">
+        <div className=" w-full inset-y-6   flex flex-row-reverse bg-blue-800 h-31">
             <div className=" flex flex-rowo m-10">
                 {/* <img src="./p/sib.jpg"></img> */}
                 <p className="text-white text-lg">های دارو</p>
             </div>
 
-            <div className="flex flex-col m-2 w-4/5 p-">
+            <div className="flex flex-col m-2 w-4/5">
                 <p className="text-white text-2xl">تامین دارو مورد نیاز هر داروخانه</p>
                 <p className=" ml-16 text-white flex"> <span className="text-yellow-600"> های دارو </span> با سامانه جامع  </p>
                 <button className= "bg-red-500 rounded text-center p-2 mt-2  text-white w-52 ml-10">ثبت نام/ ورود</button>
