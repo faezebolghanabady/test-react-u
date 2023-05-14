cf91b11864bfe7ec07cf50c6624437015e92e843
faeze <faezeebold@gmail.com>
2023-05-14 15:22:28 +0330

    test

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = (props) => {
     return(
         <div>
             
-<nav class="bg-white border-gray-200 dark:bg-slate-500 h-24">
+<nav class="bg-white border-gray-20 dark:bg-slate-500 h-24">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="https://flowbite.com/" class="flex items-center">
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">products</span>
