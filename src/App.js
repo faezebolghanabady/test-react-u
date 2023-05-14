a1540497da991e3173ac04f584c8ecbd3c4be31a
faeze <faezeebold@gmail.com>
2023-05-14 10:57:35 +0330

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
 import { render } from "@testing-library/react";
 import { Component } from "react";
 import FCounts from "./Fcounts";
+import Navbar from "./Navbar";
+import { useState } from "react";
 
 
 
 const App = () =>{
+
+    const [products , setproducts ] = useState([
+        {id:1 , count:3 , productName:'laptopp'},
+        {id:2 , count:4 , productName:'phone'},
+        {id:3 , count: 8, productName:'airpods'}
+    ])
+
     return(
         <>
-        <FCounts/>
+        <Navbar products={products}/>
+        <FCounts products={products}
+         onIncrement={handleIncrement}
+         onDelete={handleDelete}
+         onReset={handleReset}
+         oDecrement={handleDecrement}
+
+          />
         </>
+
     )
+
+
+    
+     function handleDelete(productId){
+        const newProducts = this.state.products.filter( p => p.id !== productId)
+        this.setState({products : newProducts})
+    }
+
+    function handleReset(){
+        const newProducts = this.state.products.map(p =>{
+            p.count = 0 ;
+            return p
+        });
+        this.setState({products:newProducts})
+    }
+
+    function handleIncrement (productId) {
+        const newProducts = [...this.state.products];
+        const index = newProducts.findIndex(p=> p.id === productId)
+        newProducts[index].count +=1;
+        this.setState({products : newProducts})
+    }
+
+    function handleDecrement (productId) {
+        const newProducts = [...this.state.products];
+        const index = newProducts.findIndex(p=> p.id === productId)
+        newProducts[index].count -=1;
+        this.setState({products : newProducts})
+    }
+  
+
 }
 
 export default App;
--- src/Fcounts.jsx
+++ src/Fcounts.jsx
@@ -1,26 +1,24 @@
 import FCount from "./Fcount";
 import { useState } from "react";
 
-const FCounts = () => {
+const FCounts = ({onIncrement , onDelete , onReset , onDecrement , products}) => {
 
- const [product , productName ] = useState([
-       {id:1 , count:3 , productName:'laptopp'},
-       {id:2 , count:4 , productName:'phone'},
-       {id:3 , count: 8, productName:'airpods'}
-   ])
+
 
    return(
        <div>
-           {product.map((p , index) => (
-           <FCount onDelete={handleDelete} key={index} productName={p.productName} id={p.id} count={p.count} >
+           {products.map((p , index) => (
+           <FCount onDelete={onDelete} key={index}
+            productName={p.productName}
+            id={p.id}
+            count={p.count}
+            onDecrement={onDecrement}
+            onReset={onReset}
+            onIncrement={onIncrement} >
            </FCount>
           ))}
        </div>
    )
 
-   function handleDelete (productId){
-       const newProducts = product.filter(p => p.id !== productId);
-       productName(newProducts)
-   }
 }
 export default FCounts;
