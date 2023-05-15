617b2c2d37788996a8ba015ae9ed7b1f6f689887
faeze <faezeebold@gmail.com>
2023-05-15 22:27:55 +0330

    mainPage

--- src/Fcounts.jsx
+++ src/Fcounts.jsx
@@ -7,8 +7,8 @@ const FCounts = (props) => {
     const procontext = useContext(productContext);
 
    return(
-       <div>
-        <button onClick={procontext.onReset}> reset</button>
+       <div className="p-4 border ">
+        <button className="w-16 bg-black text-white p-2" onClick={procontext.onReset}> reset</button>
            {procontext.products.map((p , index) => (
            <FCount key={index}
             productName={p.productName}
